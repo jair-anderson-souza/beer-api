88bf605f48fa3905abb7e51c5cd5cf6201c034c1
Anderson Souza <jair_anderson_bs@hotmail.com>
2019-07-02 21:35:24 -0300

    add funçoes do arquivo de configuraçao do servidor

--- server.js
+++ server.js
@@ -1,40 +1,21 @@
-//importação do express JS
 const express = require('express');
-//chamando a função d express JS
 const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
 
 const mongoose = require('mongoose');
 
 const requireDir = require('require-dir');
+requireDir('./src/models');
 
-mongoose.connect('mongodb://localhost:27017/beer', {
+const routes = require('./src/routes');
+
+mongoose.connect('mongodb://127.0.0.1:27017/beer', {
   useNewUrlParser: true
 });
 
-//mapeando a pasta de todas as entidades
-requireDir('./src/models');
+app.use('/api', routes);
 
-//chamando o schema definido no modelo
-var User = mongoose.model('User');
-
-User.create({
-  id: 1,
-  name: 'Jair Anderson',
-  username: 'jair',
-  password: '',
-  roles: [
-    {
-    }
-  ];
-});
-
-//add método GET na raiz da app
-app.get('/', (req, resp) => {
-
-
-
-  resp.send('Enviando resposta');
-});
-
-//add a porta padrão do servidor
 app.listen(3000);
